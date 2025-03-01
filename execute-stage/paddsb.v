e4fbe8a952e869107c894ad4243bed411035326d
Tyler Richard <tylerrichard215@gmail.com>
2025-03-01 09:57:13 -0600

    Finish WB and EXEC hopefully

--- execute-stage/paddsb.v
+++ execute-stage/paddsb.v
@@ -9,77 +9,28 @@ module full_adder_1bit(A, B, Cin, Sum, Cout);
 	assign Cout = (half & Cin) | (A & B);
 endmodule
 
-module addsub_4bit(Sum, Ovfl, A, B, sub);
+module addsub_4bit(A, B, Sum);
 	input [3:0] A, B;
-	input sub;
 	output [3:0] Sum;
-	output Ovfl;
 
 	wire [2:0] carry;
 	wire carry_out;
-	wire [3:0] B_mod;
 
-	assign B_mod = sub ? ~B : B;
+	wire [3:0] result;
+	full_adder_1bit FAs [3:0] (.A(A), .B(B_mod), .Sum(result), .Cin({carry, 1'b0}), .Cout({carry_out, carry}));
 
-	full_adder_1bit FAs [3:0] (.A(A), .B(B_mod), .Sum(Sum), .Cin({carry, sub}), .Cout({carry_out, carry}));
-
-	wire sign_A, sign_B, sign_Sum;
-	assign sign_A = A[3];
-	assign sign_B = sub ? ~B[3] : B[3];
-	assign sign_Sum = Sum[3];
-	assign Ovfl = (sign_A & sign_B & ~sign_Sum) | (~sign_A & ~sign_B & sign_Sum);
+	// Saturate
+	wire should_sat;
+	assign should_sat = (A[3] == B_mod[3]) && (A[3] != result[3]);
+	assign Sum = should_sat ? {A[3] ? 4'b1000 : 4'b0111} : result;
 endmodule
 
-// TODO: Make this saturate instead of overflow
-module PSA_16bit(Sum, Error, A, B);
+module PSA_16bit(A, B, Sum);
 	input [15:0] A, B;
 	output [15:0] Sum;
-	output Error;
 
-	wire [3:0] errors;
-
-	addsub_4bit add0 (.A(A[3:0]), .B(B[3:0]), .Sum(Sum[3:0]), .Ovfl(errors[0]), .sub(1'b0));
-	addsub_4bit add1 (.A(A[7:4]), .B(B[7:4]), .Sum(Sum[7:4]), .Ovfl(errors[1]), .sub(1'b0));
-	addsub_4bit add2 (.A(A[11:8]), .B(B[11:8]), .Sum(Sum[11:8]), .Ovfl(errors[2]), .sub(1'b0));
-	addsub_4bit add3 (.A(A[15:12]), .B(B[15:12]), .Sum(Sum[15:12]), .Ovfl(errors[3]), .sub(1'b0));
-
-	assign Error = |errors;
-endmodule
-
-module PSA_16bit_tb();
-	reg [15:0] A, B;
-	wire [15:0] Sum;
-	wire Error;
-
-	PSA_16bit DUT(.A(A), .B(B), .Sum(Sum), .Error(Error));
-
-	initial begin
-		A = 0;
-		B = 0;
-
-		// Test addition
-		repeat(20) begin
-			A = $random;
-			B = $random;
-			#1;
-			if((A[3:0] + B[3:0]) !== Sum[3:0]) begin
-				$display("Error in bits 0-3: %d + %d != %d", A[3:0], B[3:0], Sum[3:0]);
-				$stop();
-			end
-			if((A[7:4] + B[7:4]) !== Sum[7:4]) begin
-				$display("Error in bits 4-7: %d + %d != %d", A[7:4], B[7:4], Sum[7:4]);
-				$stop();
-			end
-			if((A[11:8] + B[11:8]) !== Sum[11:8]) begin
-				$display("Error in bits 8-11: %d + %d != %d", A[11:8], B[11:8], Sum[11:8]);
-				$stop();
-			end
-			if((A[15:12] + B[15:12]) !== Sum[15:12]) begin
-				$display("Error in bits 12-15: %d + %d != %d", A[15:12], B[15:12], Sum[15:12]);
-				$stop();
-			end
-		end
-
-		$display("Success!!!");
-	end
+	addsub_4bit add0 (.A(A[3:0]), .B(B[3:0]), .Sum(Sum[3:0]));
+	addsub_4bit add1 (.A(A[7:4]), .B(B[7:4]), .Sum(Sum[7:4]));
+	addsub_4bit add2 (.A(A[11:8]), .B(B[11:8]), .Sum(Sum[11:8]));
+	addsub_4bit add3 (.A(A[15:12]), .B(B[15:12]), .Sum(Sum[15:12]));
 endmodule
--- writeback-stage/writeback_stage.v
+++ writeback-stage/writeback_stage.v
@@ -8,17 +8,18 @@ module writeback_stage(
 	// Assign reg write data
 	assign reg_write_data = reg_write_src ? mem_read : alu_result;
 	// Assign next PC
-	wire should_branch;
+	wire should_branch, greater_than;
+	assign greater_than = (flags[2] == flags[1]) && (flags[2] == 1'b0);
 	always @* begin
 		case(branch_cond)
-			3'b000: should_branch = flags[1] == 1'b0;									// Not Equal (Z = 0)
-			3'b001: should_branch = flags[1] == 1'b1;									// Equal (Z = 1)
-			3'b010: should_branch = flags[2] == flags[1] == 1'b0;						// Greater Than (Z = N = 0)
-			3'b011: should_branch = flags[2] == 1'b1;									// Less Than (N = 1)
-			3'b100: should_branch = (flags[1] == 1'b1) | (flags[2] == flags[1] == 1'b0);// Greater Than or Equal (Z = 1 or Z = N = 0)
-			3'b101: should_branch = (flags[2] == 1'b1) | (flags[1] == 1'b1);			// Less Than or Equal (N = 1 or Z = 1)
-			3'b110: should_branch = flags[0] == 1'b1;									// Overflow (V = 1)
-			3'b111: should_branch = 1'b1;												// Unconditional
+			3'b000: should_branch = flags[1] == 1'b0;							// Not Equal (Z = 0)
+			3'b001: should_branch = flags[1] == 1'b1;							// Equal (Z = 1)
+			3'b010: should_branch = greater_than;								// Greater Than (Z = N = 0)
+			3'b011: should_branch = flags[2] == 1'b1;							// Less Than (N = 1)
+			3'b100: should_branch = (flags[1] == 1'b1) | greater_than;			// Greater Than or Equal (Z = 1 or Z = N = 0)
+			3'b101: should_branch = (flags[2] == 1'b1) | (flags[1] == 1'b1);	// Less Than or Equal (N = 1 or Z = 1)
+			3'b110: should_branch = flags[0] == 1'b1;							// Overflow (V = 1)
+			3'b111: should_branch = 1'b1;										// Unconditional
 		endcase
 	end
 	assign next_pc = should_branch ? alu_result : pc_plus2;
